edcf2f96845c7a1b5e5f54df32c9dab06113c316
itogi <32363585+itogi@users.noreply.github.com>
2022-08-10 13:12:34 +0300

    Update sketch.js

--- 2x2/clock/sketch.js
+++ 2x2/clock/sketch.js
@@ -4,7 +4,7 @@ var w=720; //Высота экрана
 function setup() {
   canvas = createCanvas(h, w); //Создание экрана с заданной шириной и высотой
   angleMode(DEGREES);
-  clockNum = loadFont('/COOPBL.ttf');
+  clockNum = loadFont('/COOPBL.otf');
 }
 
 function draw() {
